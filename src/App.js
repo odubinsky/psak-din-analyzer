65a86e209b4d46ce133feefab581fb5f5885a856
Ori Dubinsky <orid@wix.com>
2019-03-10 01:43:56 +0200

    made order

--- src/App.js
+++ src/App.js
@@ -5,49 +5,42 @@ import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import {Dropdown} from 'primereact/dropdown';
-import {Chart} from 'primereact/chart';
 import {TabMenu} from 'primereact/tabmenu';
 import judges from './judges'
-import authornies from './authornies'
-import {DataTable} from 'primereact/datatable';
-import {Column} from 'primereact/column';
-
-
+import lawyers from './lawyers'
+import Visualization from './VIsualization.js'
 
 class App extends Component {
-
-  
   constructor() {
     super();
-    this.currentTab = this.currentTab.bind(this);
     this.state = {
         judge: {label: 'כלום', value: 'כלום'}, 
-        authorny: {label: 'כלום', value: 'כלום'},
+        lawyer: {label: 'כלום', value: 'כלום'},
         tab: {label: 'כלום'}
     };
     this.judges = judges;
-    this.authornies = authornies;
-    this.judgesArr = Object.entries(judges).sort((a,b) => a[1]['accepted_ratio: ']  > b[1]['accepted_ratio: '] ? 1 : -1).slice(-10);
-    this.judgesArr = this.judgesArr.map(this.mapToTable);
-    this.authorniesArr = Object.entries(authornies).sort((a,b) => a[1]['accepted_ratio: ']  > b[1]['accepted_ratio: '] ? 1 : -1).slice(-10);
-    this.authorniesArr = this.authorniesArr.map(this.mapToTable)
+    this.lawyers = lawyers;
+    this.judgesArr = Object.entries(judges).sort((a,b) => a[1]['accepted_ratio: ']  > b[1]['accepted_ratio: '] ? 1 : -1).slice(-10).map(this.mapToTable);
+    this.lawyersArr = Object.entries(lawyers).sort((a,b) => a[1]['accepted_ratio: ']  > b[1]['accepted_ratio: '] ? 1 : -1).slice(-10).map(this.mapToTable);
     this.tabs = [{label : 'כלום'},{label : 'עמודות'},{label : 'עיגול'},{label : 'פסקי דין'},{label : 'שופטים מומלצים'},{label : 'עו״ד מומלצים'}];
     this.judgesKeys = [];
-    this.authorniesKeys = [];
+    this.lawyersKeys = [];
+    this.createDropdownLists();
+    this.onJudgeChange = this.onJudgeChange.bind(this);
+    this.onAuthChange = this.onAuthChange.bind(this);
+  }
 
+  createDropdownLists() {
     for (var j in this.judges) {
       if(this.judges[j]['accepted_ratio: '] > 0 ){
         this.judgesKeys.push({label: j, value: j});
       }
     };
-    for (var a in this.authornies) {
-      if(this.authornies[a]['accepted_ratio: '] > 0) {
-        this.authorniesKeys.push({label: a, value: a});
+    for (var a in this.lawyers) {
+      if(this.lawyers[a]['accepted_ratio: '] > 0) {
+        this.lawyersKeys.push({label: a, value: a});
       }
     };
-    
-    this.onJudgeChange = this.onJudgeChange.bind(this);
-    this.onAuthChange = this.onAuthChange.bind(this);
   }
 
   onJudgeChange(e) {
@@ -55,7 +48,7 @@ class App extends Component {
   }
 
   onAuthChange(e) {
-    this.setState({authorny: e.value});
+    this.setState({lawyer: e.value});
   }
 
   onTabChange(e) {
@@ -63,117 +56,40 @@ class App extends Component {
   }
 
   mapToTable(entry){
-    entry[1].name = entry[0];
-    return entry[1];
-  }
-
-  currentTab() {
-    if(this.state.tab.label === 'עמודות'){
-      const chosenJudgeRatios = this.judges[`${this.state.judge}`]
-      const chosenAuthornyRatios = this.authornies[`${this.state.authorny}`]
-      const chartObj = {
-        labels: [],
-        datasets: [{
-          label: 'אחוזי הצלחה', 
-          backgroundColor: '#42A5F5',
-          data: []
-        },{
-          label: 'אחוזי דחייה',
-          backgroundColor: '#9CCC65',
-          data: []
-        }]
-      }
-      if(chosenJudgeRatios) {
-        chartObj.labels.push(`${this.state.judge}`)
-        chartObj.datasets[0].data.push(chosenJudgeRatios['accepted_ratio: ']);
-        chartObj.datasets[1].data.push(chosenJudgeRatios['rejected_ratio: ']);
-      }
-      if (chosenAuthornyRatios) {
-        chartObj.labels.push(`${this.state.authorny}`)
-        chartObj.datasets[0].data.push(chosenAuthornyRatios['accepted_ratio: ']);
-        chartObj.datasets[1].data.push(chosenAuthornyRatios['rejected_ratio: ']);
-      }
-      return <Chart type="bar" data={chartObj} style={{width: '600px', left: 'calc(50% - 300px)', top: '20px'}} />
-    }
-    if(this.state.tab.label === 'עיגול'){
-      const chosenJudgeRatios = this.judges[`${this.state.judge}`]
-      const chosenAuthornyRatios = this.authornies[`${this.state.authorny}`]
-      let chartObjJudge, chartObjAuth;
-      chartObjJudge = {
-        labels: ['אחוזי קבלה','אחוזי דחייה','אחר'],
-        datasets: [{
-          backgroundColor: ['#42A5F5','#9CCC65','red'],
-          data: []
-        }]
-      }
-      chartObjAuth  = {
-        labels: ['אחוזי קבלה','אחוזי דחייה','אחר'],
-        datasets: [{
-          backgroundColor: ['#42A5F5','#9CCC65','red'],
-          data: []
-        }]
-      }
-      if(chosenJudgeRatios) {
-        let ratio = [chosenJudgeRatios['accepted_ratio: '],chosenJudgeRatios['rejected_ratio: ']]
-        chartObjJudge.datasets[0].data.push(ratio[0]);
-        chartObjJudge.datasets[0].data.push(ratio[1]);
-        chartObjJudge.datasets[0].data.push(1 - ratio[0] - ratio[1]);
-      }
-      if (chosenAuthornyRatios) {
-        let ratio = [chosenAuthornyRatios['accepted_ratio: '],chosenAuthornyRatios['rejected_ratio: ']]
-        chartObjAuth.datasets[0].data.push(ratio[0]);
-        chartObjAuth.datasets[0].data.push(ratio[1]);
-        chartObjAuth.datasets[0].data.push(1 - ratio[0] - ratio[1]);
-      }
-      return (
-      <div style={{top: '40px', position: 'relative'}}>
-        <Chart type="doughnut" data={chartObjJudge} style={{width: '50%', position: 'absolute'}} />
-        <Chart type="doughnut" data={chartObjAuth} style={{width: '50%', left: '50%', position: 'absolute'}} />
-      </div>)
-    }
-    if(this.state.tab.label ===  'עו״ד מומלצים') {
-      return (<DataTable value={this.authorniesArr}>
-        <Column field="rejected_ratio: " header="Rejected Ratio" />
-        <Column field="accepted_ratio: " header="Accepted Ratio" />
-        <Column field="name" header="שם" />
-      </DataTable>)
-    }
-    if(this.state.tab.label ===  'שופטים מומלצים') {
-      return (<DataTable value={this.judgesArr}>
-        <Column field="rejected_ratio: " header="אחוז דחייה" />
-        <Column field="accepted_ratio: " header="אחוז קבלה" />
-        <Column field="name" header="שם" />
-      </DataTable>)
+    return {
+      name: entry[0],
+      accepted_ratio: entry[1]['accepted_ratio: '] * 100,
+      rejected_ratio: entry[1]['rejected_ratio: '] * 100
     }
   }
 
-  isPickEmpty() {
-    if(this.state.judge && this.state.judge.label !== 'כלום' ) {
-      return false;
-    }
-    if(this.state.authorny && this.state.authorny.label !== 'כלום' ) {
-      return false;
-    }
-    if(this.state.tab.label === 'שופטים מומלצים' || this.state.tab.label === 'עו״ד מומלצים'){
-      return false;
-    }
-    return true;
-  }
 
   render() {
+    const visProps = {
+      tab: this.state.tab.label,
+      chosenJudge: this.state.judge,
+      chosenLawyer: this.state.lawyer,
+      judges: this.judges,
+      judgesArr: this.judgesArr,
+      lawyers: this.lawyers,
+      lawyersArr: this.lawyersArr
+    };
     return (
       <div className="App">
         <header className="App-header">
           ניתוח פסקי דין
         </header>
+        <div className="avg-section">
+
+        </div>
         <div className="pick-section">
           <Dropdown value={this.state.judge} options={this.judgesKeys} onChange={this.onJudgeChange}  style={{width:'300px', left: '-40px', direction: 'rtl'}} placeholder={"בחר שופט"} filter={true} showClear={true}/>
-          <Dropdown value={this.state.authorny} options={this.authorniesKeys} onChange={this.onAuthChange}  style={{width:'300px', left: '40px'}} placeholder={"בחר עו״ד"} filter={true} showClear={true}/>
+          <Dropdown value={this.state.lawyer} options={this.lawyersKeys} onChange={this.onAuthChange}  style={{width:'300px', left: '40px'}} placeholder={"בחר עו״ד"} filter={true} showClear={true}/>
         </div>
 
         <div className="visualization-section" style={{width: '1000px', left: 'calc(50% - 500px)'}}>
           <TabMenu model={this.tabs} activeItem={this.state.tab} onTabChange={(e) => this.setState({tab: e.value})} style={{width: '1000px', float: 'right !important'}}/>
-          {this.isPickEmpty() ? '' : this.currentTab()}
+          <Visualization {...visProps}/>
         </div>
       </div>
     );
--- src/VIsualization.js
+++ src/VIsualization.js
@@ -1,82 +1,107 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {Chart} from 'primereact/chart';
 
 
-// class Visualization extends Component {
-//   constructor(props) {
-//     super(props);
+class Visualization extends Component {
+  isEmpty(chosenJudge, chosenLawyer, tab) {
+    if(chosenJudge && chosenJudge.label !== 'כלום' ) {
+      return false;
+    }
+    if(chosenLawyer && chosenLawyer.label !== 'כלום' ) {
+      return false;
+    }
+    if(tab === 'שופטים מומלצים' || tab === 'עו״ד מומלצים'){
+      return false;
+    }
+    return true;
+  }
+  
 
-    
-
-    
-//   }
-
-//   render(){
-//     if(this.state.tab.label === 'עמודות'){
-//     const chosenJudgeRatios = this.judges[`${this.state.judge}`]
-//     const chosenAuthornyRatios = this.authornies[`${this.state.authorny}`]
-//     const chartObj = {
-//       labels: [],
-//       datasets: [{
-//         label: 'אחוזי הצלחה', 
-//         backgroundColor: '#42A5F5',
-//         data: []
-//       },{
-//         label: 'אחוזי דחייה',
-//         backgroundColor: '#9CCC65',
-//         data: []
-//       }]
-//     }
-//     if(chosenJudgeRatios) {
-//       chartObj.labels.push(`${this.state.judge}`)
-//       chartObj.datasets[0].data.push(chosenJudgeRatios['accepted_ratio: ']);
-//       chartObj.datasets[1].data.push(chosenJudgeRatios['rejected_ratio: ']);
-//     }
-//     if (chosenAuthornyRatios) {
-//       chartObj.labels.push(`${this.state.authorny}`)
-//       chartObj.datasets[0].data.push(chosenAuthornyRatios['accepted_ratio: ']);
-//       chartObj.datasets[1].data.push(chosenAuthornyRatios['rejected_ratio: ']);
-//     }
-//     return <Chart type="bar" data={chartObj} style={{width: '600px', left: 'calc(50% - 300px)', top: '20px'}} />
-//   }
-//   if(this.state.tab.label === 'עיגול'){
-//     const chosenJudgeRatios = this.judges[`${this.state.judge}`]
-//     const chosenAuthornyRatios = this.authornies[`${this.state.authorny}`]
-//     let chartObjJudge, chartObjAuth;
-//     chartObjJudge = {
-//       labels: ['אחוזי הצלחה','אחוזי דחייה','אחר'],
-//       datasets: [{
-//         backgroundColor: ['#42A5F5','#9CCC65','red'],
-//         data: []
-//       }]
-//     }
-//     chartObjAuth  = {
-//       labels: ['אחוזי הצלחה','אחוזי דחייה','אחר'],
-//       datasets: [{
-//         backgroundColor: ['#42A5F5','#9CCC65','red'],
-//         data: []
-//       }]
-//     }
-//     if(chosenJudgeRatios) {
-//       let ratio = [chosenJudgeRatios['accepted_ratio: '],chosenJudgeRatios['rejected_ratio: ']]
-//       chartObjJudge.datasets[0].data.push(ratio[0]);
-//       chartObjJudge.datasets[0].data.push(ratio[1]);
-//       chartObjJudge.datasets[0].data.push(1 - ratio[0] - ratio[1]);
-//     }
-//     if (chosenAuthornyRatios) {
-//       let ratio = [chosenAuthornyRatios['accepted_ratio: '],chosenAuthornyRatios['rejected_ratio: ']]
-//       chartObjAuth.datasets[0].data.push(ratio[0]);
-//       chartObjAuth.datasets[0].data.push(ratio[1]);
-//       chartObjAuth.datasets[0].data.push(1 - ratio[0] - ratio[1]);
-//     }
-//     return (
-//     <div style={{top: '40px', position: 'relative'}}>
-//       <Chart type="doughnut" data={chartObjJudge} style={{width: '50%', position: 'absolute'}} />
-//       <Chart type="doughnut" data={chartObjAuth} style={{width: '50%', left: '50%', position: 'absolute'}} />
-//     </div>)
-//   }
-//     return <div></div>
-//   }
-
-// }
-
-// export default Visualization
+  render() {
+    const {judges, lawyers, chosenJudge, chosenLawyer, judgesArr, lawyersArr, tab} = this.props
+    if(this.isEmpty(chosenJudge, chosenLawyer, tab)) {
+      return null;
+    }
+    if(tab === 'עמודות') {
+      const chosenJudgeRatios = judges[`${chosenJudge}`]
+      const chosenLawyerRatios = lawyers[`${chosenLawyer}`]
+      const chartObj = {
+        labels: [],
+        datasets: [{
+          label: 'אחוזי הצלחה', 
+          backgroundColor: '#42A5F5',
+          data: []
+        },{
+          label: 'אחוזי דחייה',
+          backgroundColor: '#9CCC65',
+          data: []
+        }]
+      }
+      if(chosenJudgeRatios) {
+        chartObj.labels.push(`${chosenJudge}`)
+        chartObj.datasets[0].data.push(chosenJudgeRatios['accepted_ratio: '] * 100);
+        chartObj.datasets[1].data.push(chosenJudgeRatios['rejected_ratio: '] * 100);
+      }
+      if (chosenLawyerRatios) {
+        chartObj.labels.push(`${chosenLawyer}`)
+        chartObj.datasets[0].data.push(chosenLawyerRatios['accepted_ratio: '] * 100);
+        chartObj.datasets[1].data.push(chosenLawyerRatios['rejected_ratio: '] * 100);
+      }
+      return <Chart type="bar" data={chartObj} style={{width: '600px', left: 'calc(50% - 300px)', top: '20px'}} />
+    }
+    if(tab === 'עיגול'){
+      const chosenJudgeRatios = judges[`${chosenJudge}`]
+      const chosenLawyerRatios = lawyers[`${chosenLawyer}`]
+      let chartObjJudge, chartObjAuth;
+      chartObjJudge = {
+        labels: ['אחוזי קבלה','אחוזי דחייה','אחר'],
+        datasets: [{
+          backgroundColor: ['#42A5F5','#9CCC65','red'],
+          data: []
+        }]
+      }
+      chartObjAuth  = {
+        labels: ['אחוזי קבלה','אחוזי דחייה','אחר'],
+        datasets: [{
+          backgroundColor: ['#42A5F5','#9CCC65','red'],
+          data: []
+        }]
+      }
+      if(chosenJudgeRatios) {
+        let ratio = [chosenJudgeRatios['accepted_ratio: '] * 100, chosenJudgeRatios['rejected_ratio: '] * 100]
+        chartObjJudge.datasets[0].data.push(ratio[0]);
+        chartObjJudge.datasets[0].data.push(ratio[1]);
+        chartObjJudge.datasets[0].data.push(100 - ratio[0] - ratio[1]);
+      }
+      if (chosenLawyerRatios) {
+        let ratio = [chosenLawyerRatios['accepted_ratio: '] * 100, chosenLawyerRatios['rejected_ratio: '] * 100]
+        chartObjAuth.datasets[0].data.push(ratio[0]);
+        chartObjAuth.datasets[0].data.push(ratio[1]);
+        chartObjAuth.datasets[0].data.push(100 - ratio[0] - ratio[1]);
+      }
+      return (
+      <div style={{top: '40px', position: 'relative'}}>
+        <Chart type="doughnut" data={chartObjJudge} style={{width: '50%', position: 'absolute'}} />
+        <Chart type="doughnut" data={chartObjAuth} style={{width: '50%', left: '50%', position: 'absolute'}} />
+      </div>)
+    }
+    if(tab ===  'עו״ד מומלצים') {
+      return (<DataTable value={lawyersArr}>
+        <Column field="rejected_ratio" header="אחוז דחייה" />
+        <Column field="accepted_ratio" header="אחוז קבלה" />
+        <Column field="name" header="שם" />
+      </DataTable>)
+    }
+    if(tab ===  'שופטים מומלצים') {
+      return (<DataTable value={judgesArr}>
+        <Column field="rejected_ratio" header="אחוז דחייה" />
+        <Column field="accepted_ratio" header="אחוז קבלה" />
+        <Column field="name" header="שם" />
+      </DataTable>)
+    }
+    return null;
+  }
+}
+export default Visualization
