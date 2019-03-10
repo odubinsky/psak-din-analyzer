6dca3a6972f5799c771dad04c1c854c771cbc1db
Ori Dubinsky <orid@wix.com>
2019-03-10 22:07:36 +0200

    Updates

--- precache-manifest.d4c71312a936d7960d6afaaafc615c70.js
+++ precache-manifest.d4c71312a936d7960d6afaaafc615c70.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "bf2d0783515b7d75c35bde69e01b3135",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
   },
   {
-    "revision": "e16bbfe819584e16166d",
+    "revision": "9d797019af29a0097307",
     "url": "/psak-din-analyzer/static/css/main.0c0bc06c.chunk.css"
   },
   {
@@ -20,16 +20,12 @@ self.__precacheManifest = [
     "url": "/psak-din-analyzer/static/js/2.c1ca5f5a.chunk.js"
   },
   {
-    "revision": "60c866748ff15f5b347fdba64596b1b1",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.60c86674.woff2"
-  },
-  {
     "revision": "76b56857ebbae3a5a689f213feb11af0",
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.76b56857.eot"
   },
   {
-    "revision": "177cc92d2e8027712a8c1724abd272cd",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.60c86674.woff2"
   },
   {
     "revision": "521d17bc9f3526c690e8ada6eee55bec",
@@ -40,10 +36,6 @@ self.__precacheManifest = [
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.27ef0b06.svg"
   },
   {
-    "revision": "9dce7f01715340861bdb57318e2f3fdc",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
-  },
-  {
     "revision": "cffb686d7d2f4682df8342bd4d276e09",
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
   },
@@ -52,32 +44,40 @@ self.__precacheManifest = [
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
   },
   {
-    "revision": "e16bbfe819584e16166d",
-    "url": "/psak-din-analyzer/static/js/main.e16bbfe8.chunk.js"
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
   },
   {
     "revision": "148a6749baa5f658a45183ddb5ee159f",
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.148a6749.eot"
   },
   {
-    "revision": "7aab4c13671282c90669eb6a10357e41",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
-  },
-  {
-    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+    "revision": "9d797019af29a0097307",
+    "url": "/psak-din-analyzer/static/js/main.9d797019.chunk.js"
   },
   {
     "revision": "623e3205570002af47fc2b88f9335d19",
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.623e3205.woff"
   },
   {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
     "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
     "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.2e00b263.svg"
   },
   {
-    "revision": "7e08cc656863d52bcb5cd34805ac605b",
-    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/psak-din-analyzer/static/media/open-sans-v15-latin-300.177cc92d.ttf"
   },
   {
     "revision": "c7a33805ffda0d32bd2a9904c8b02750",
@@ -104,7 +104,7 @@ self.__precacheManifest = [
     "url": "/psak-din-analyzer/static/css/2.803c4049.chunk.css"
   },
   {
-    "revision": "0f4c35dfbcf842b5c48ed1678dc50782",
+    "revision": "29a07734506aa9978d8a38e8cee408e6",
     "url": "/psak-din-analyzer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/psak-din-analyzer/precache-manifest.e775c6ae94ce3f99cba7791f32f089ec.js"
+  "/psak-din-analyzer/precache-manifest.d4c71312a936d7960d6afaaafc615c70.js"
 );
 
 workbox.clientsClaim();
